c7df6eddc92c3b43101bcbced79bfb6748876fd9
Sang14121315 <huynhsangchovam@gmail.com>
2025-06-12 00:55:25 +0700

    thiẹn
    s

--- frontend/src/layouts/auth.layout.tsx
+++ frontend/src/layouts/auth.layout.tsx
@@ -1,16 +1,16 @@
-import Footer from "@/components/footer/footer";
-import Header from "@/components/header/header";
-import React from "react";
-import { Outlet } from "react-router-dom";
+// import Footer from "@/components/footer/footer";
+// import Header from "@/components/header/header";
+// import React from "react";
+// import { Outlet } from "react-router-dom";
 
-const AuthLayout: React.FC = () => {
-  return (
-    <div>
-      <Header />
-      <Outlet /> 
-      <Footer />
-    </div>
-  );
-};
+// const AuthLayout: React.FC = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Outlet /> 
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default AuthLayout;
+// export default AuthLayout;
--- frontend/src/router/index.router.tsx
+++ frontend/src/router/index.router.tsx
@@ -1,10 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "@/layouts/auth.layout";
+import { Routes, Route } from "react-router-dom";
+import AdminLayout from "@/layouts/admin.layout";
+import DashboardPage from "@/pages/admin/Dashboard";
+// import AuthLayout from "@/layouts/auth.layout";
+
+// Các page giả định
 
 const MainRouter = () => {
   return (
     <Routes>
-      <Route element={<AuthLayout />}>
+     
+      {/* Admin layout */}
+      <Route path="/admin" element={< AdminLayout />}>
+      <Route path="dashboard" element={<DashboardPage />} />
       </Route>
     </Routes>
   );
